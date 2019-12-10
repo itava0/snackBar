f3e33588efa93001f6e2275e2e06a7d9a6cfdc48
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-12-10 17:17:41 -0500

    Create getters and setters for VendingMachine

--- src/snackBar/VendingMachine.java
+++ src/snackBar/VendingMachine.java
@@ -13,4 +13,22 @@ public class VendingMachine {
         id = maxId;
         this.name = name;
     }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
